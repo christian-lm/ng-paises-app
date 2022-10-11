f33b240ac216786281e7b7ed899143d62eef2913
Christian Lozano <christian.lozano@neoris.com>
2022-10-11 19:11:11 +0200

    - Reformat general - Se mueve el sidebard a su componente

--- src/app/paises/paises.module.ts
+++ src/app/paises/paises.module.ts
@@ -1,10 +1,9 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { PorCapitalComponent } from './pages/por-capital/por-capital.component';
-import { PorPaisComponent } from './pages/por-pais/por-pais.component';
-import { PorRegionComponent } from './pages/por-region/por-region.component';
-import { VerPaisComponent } from './pages/ver-pais/ver-pais.component';
-
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {PorCapitalComponent} from './pages/por-capital/por-capital.component';
+import {PorPaisComponent} from './pages/por-pais/por-pais.component';
+import {PorRegionComponent} from './pages/por-region/por-region.component';
+import {VerPaisComponent} from './pages/ver-pais/ver-pais.component';
 
 
 @NgModule({
@@ -24,4 +23,5 @@ import { VerPaisComponent } from './pages/ver-pais/ver-pais.component';
     CommonModule
   ]
 })
-export class PaisesModule { }
+export class PaisesModule {
+}
