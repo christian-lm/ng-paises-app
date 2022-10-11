a22121aaefd720e5359490b012d4a8e6f88e5ebd
Christian Lozano <christian.lozano@neoris.com>
2022-10-11 13:33:24 +0200

    - Se empiezan a configurar las rutas de la aplicacion

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,42 @@
+import {NgModule} from "@angular/core";
+import {RouterModule, Routes} from "@angular/router";
+import {PorPaisComponent} from "./paises/pages/por-pais/por-pais.component";
+import {PorRegionComponent} from "./paises/pages/por-region/por-region.component";
+import {PorCapitalComponent} from "./paises/pages/por-capital/por-capital.component";
+import {VerPaisComponent} from "./paises/pages/ver-pais/ver-pais.component";
+
+const routes: Routes = [
+  {
+    path: '', // ruta vacia -> localhost:9090/
+    component: PorPaisComponent,
+    pathMatch: 'full' // En cualquier caso ve a este componente
+  },
+  {
+    path: 'region', // ruta region -> localhost:9090/region
+    component: PorRegionComponent,
+  },
+  {
+    path: 'capital', // ruta capital -> localhost:9090/capital
+    component: PorCapitalComponent,
+  },
+  {
+    path: 'pais/:id', // ruta pais/id -> localhost:9090/pais/1
+    component: VerPaisComponent,
+  },
+  {
+    path: '**', // En caso de que vayas a cualquier otra ruta...
+    redirectTo: '' // Redirige al pah vacio
+    //component: 404Component -> En caso de URL no existe y mostrar un error personalizado
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes) // Le paso al modulo de rutas nuestras rutas
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {PaisesModule} from "./paises/paises.module";
 import {SharedModule} from "./shared/shared.module";
+import {AppRoutingModule} from "./app-routing.module";
 
 @NgModule({
   declarations: [
@@ -11,6 +12,7 @@ import {SharedModule} from "./shared/shared.module";
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     PaisesModule,
     SharedModule
   ],
