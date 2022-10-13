b671b32bc9f52921ffacd02da56500d8da757b90
Christian Lozano <christian.lozano@neoris.com>
2022-10-13 15:35:38 +0200

    - Se crea onDebounce para el input. Preparación de sugerencias

--- src/app/paises/components/pais-input/pais-input.component.ts
+++ src/app/paises/components/pais-input/pais-input.component.ts
@@ -1,17 +1,34 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {debounceTime, Subject} from "rxjs";
 
 @Component({
   selector: 'app-pais-input',
   templateUrl: './pais-input.component.html'
 })
-export class PaisInputComponent {
+export class PaisInputComponent implements OnInit {
 
-  @Output() onEnter: EventEmitter<string> = new EventEmitter<string>();
+  @Output() onEnter: EventEmitter<string> = new EventEmitter<string>(); // Se exporta el evento del enter
+  @Output() onDebounce: EventEmitter<string> = new EventEmitter<string>();
+
+  debouncer: Subject<string> = new Subject<string>();
+
   termino: string = '';
+
+  // Se dispara solo cuando se crea el componente
+  ngOnInit(): void {
+    this.debouncer
+      .pipe(debounceTime(300)) // Solo emite valores en periodos de 300ms desde que se deja de teclear
+      .subscribe(valor => {
+        this.onDebounce.emit(valor);
+      });
+  }
 
   buscar() {
     this.onEnter.emit(this.termino);
     console.log(this.termino);
   }
 
+  teclaPresionada() {
+    this.debouncer.next(this.termino);
+  }
 }
