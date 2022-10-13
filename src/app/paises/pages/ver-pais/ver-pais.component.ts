385850eef240e4548a425f7cdc2680b73cf51fdf
Christian Lozano <christian.lozano@neoris.com>
2022-10-13 16:23:01 +0200

    - Se implementa la pantalla con los datos de un país

--- src/app/paises/pages/ver-pais/ver-pais.component.ts
+++ src/app/paises/pages/ver-pais/ver-pais.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {PaisService} from "../../services/pais.service";
+import {switchMap, tap} from "rxjs";
+import {Pais} from "../../interfaces/pais.interface";
 
 @Component({
   selector: 'app-ver-pais',
@@ -12,16 +14,28 @@ export class VerPaisComponent implements OnInit {
               private paisService: PaisService) {
   }
 
+  pais!: Pais; // Puede ser nulo pero para typescript no
+
   ngOnInit(): void {
 
     // Nos suscribimos al observable de la ruta activa
-    this.activatedRoute.params
+    /*this.activatedRoute.params
       .subscribe(({id}) => {
         this.paisService.buscarPaisPorCode(id)
           .subscribe(paises => {
             let pais = paises[0];
             console.log(pais);
           });
+      })*/
+
+    // Forma alternativa para evitar dos suscribe
+    this.activatedRoute.params
+      .pipe(
+        switchMap(({id}) => this.paisService.buscarPaisPorCode(id)),
+        tap(console.log) // El tap recibe el producto del switchMap y pinta en consola lo que reciba
+      )
+      .subscribe(paises => {
+        this.pais = paises[0];
       })
   }
 }
