f770a03c495d3c7535a21fb65ad01647bf852e77
Christian Lozano <christian.lozano@neoris.com>
2022-10-13 16:54:25 +0200

    - Se añade estilo condicional a la pantalla de busqueda por regiones

--- src/app/paises/pages/por-region/por-region.component.ts
+++ src/app/paises/pages/por-region/por-region.component.ts
@@ -1,15 +1,27 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-por-region',
-  templateUrl: './por-region.component.html'
+  templateUrl: './por-region.component.html',
+  styles: [
+    `
+      button {
+        margin-right: 5px;
+      }
+    `
+  ]
 })
-export class PorRegionComponent implements OnInit {
+export class PorRegionComponent {
 
-  constructor() {
+  regiones: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
+  regionActiva: string = '';
+
+  getClaseCSS(region: string): string {
+    return region === this.regionActiva ? 'btn btn-primary' : 'btn btn-outline-primary';
   }
 
-  ngOnInit(): void {
+  activarRegion(region: string) {
+    this.regionActiva = region;
   }
 
 }
